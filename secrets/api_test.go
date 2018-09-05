d6622466bf637bbef5dd4f1ef3c1c3dd9f4a5a4e
Sune Keller <absukl@almbrand.dk>
2018-09-05 13:24:50 +0200

    Add test
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- secrets/api_test.go
+++ secrets/api_test.go
@@ -43,6 +43,16 @@ func TestHandler(t *testing.T) {
 	if resp.Err == "" {
 		t.Fatalf("expected missing secret")
 	}
+	resp, err = pluginRequest(client, getPath, Request{SecretName: "another-secret", SecretLabels: map[string]string{"prefix": "p-"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.Err != "" {
+		t.Fatalf("error while getting secret: %v", resp.Err)
+	}
+	if !bytes.EqualFold(append([]byte("p-"), secret...), resp.Value) {
+		t.Fatalf("expecting secret value %s, got %s", secret, resp.Value)
+	}
 }
 
 func pluginRequest(client *http.Client, method string, req Request) (*Response, error) {
@@ -73,6 +83,9 @@ func (p *testPlugin) Get(req Request) Response {
 	p.get++
 	if req.SecretName == "" {
 		return Response{Err: "missing secret name"}
+	}
+	if prefix, exists := req.SecretLabels["prefix"]; exists {
+		return Response{Value: append([]byte(prefix), secret...)}
 	}
 	return Response{Value: secret}
 }
